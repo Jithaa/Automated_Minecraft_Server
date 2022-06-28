fda19ff2bf53961cbd75f03076c308c3b96afae4
Jithaamithran S <72763591+Jithaa@users.noreply.github.com>
2022-06-28 11:46:18 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import os
 os.chdir("/")
-os.chdir("/home/sjm/Documents/forge_server")
+os.chdir("/home/sjm/Documents/forge_server")#Server file location
 k=os.system("./ngrok tcp -region=ap 25565")
 
